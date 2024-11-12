956b96580e06d4f45152496b5a741070493cde04
Mehedi Hasan <{ID}+{username}@users.noreply.github.com>
2024-11-12 11:04:38 +0600

    feat: add global error handling and request validation middleware, implement ApiError class, and include Zod for schema validation

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import router from "./routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 const app: Application = express();
 
 app.use(cors());
@@ -16,5 +17,15 @@ app.get("/", (req: Request, res: Response) => {
     message: "The server is running",
   });
 });
-
+app.use(globalErrorHandler);
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: "API NOT FOUND!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
+  });
+});
 export default app;
